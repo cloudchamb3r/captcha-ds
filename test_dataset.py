85b789955e1e99a2a5f98964c149dff3bad6d297
cloudchamb3r <jizon0123@protonmail.com>
2024-10-28 15:02:57 +0900

    add pytest for data validation

--- test_dataset.py
+++ test_dataset.py
@@ -1,15 +1,10 @@
 import os 
 from functools import reduce
-import logging
 import hashlib
+import warnings
 
-### Constatnts
 CHECK_DIRS = ['./01.working', './02.labeled', './03.verified']
-EXPECTED_TOTAL_PNG_COUNT = 5002 
-
-### logging
-logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+EXPECTED_TOTAL_PNG_COUNT = 5002
 
 
 def get_png_count(dirname) -> int:
@@ -17,26 +12,21 @@ def get_png_count(dirname) -> int:
     for _,_,files in os.walk(dirname):
         acc += len(list(filter(lambda x: x.endswith('.png'), files)))
     return acc
-        
-def check_total_png_count(): 
-    logger.debug('=== CHECK TOTAL PNG COUNT ===')
-    total_png_count = reduce(lambda x, y: x + y, map(get_png_count,  CHECK_DIRS), 0)    
-    
-    logger.debug('acutal total png count: %d', total_png_count)
-    logger.debug('expected total png count: %d', EXPECTED_TOTAL_PNG_COUNT)
-    assert(total_png_count == EXPECTED_TOTAL_PNG_COUNT)
 
 def get_file_hash(filepath) -> str: 
     data = None 
     with open(filepath, 'rb') as f: 
         data = f.read()
     if data == None: 
-        logger.warning(f'get_file_hash failed on {filepath}, data cannot be None')
+        warnings.warn(f'get_file_hash failed on {filepath}, data cannot be None')
         return None
     return f'{hashlib.md5(data).hexdigest()}:{hashlib.sha256(data).hexdigest()}'
+        
+def test_total_png_count(): 
+    total_png_count = reduce(lambda x, y: x + y, map(get_png_count,  CHECK_DIRS), 0)    
+    assert(total_png_count == EXPECTED_TOTAL_PNG_COUNT)
 
-def check_dup_files():
-    logger.debug('=== CHECK DUP FILES ===')
+def test_dup_files():
     dupdict = {}  
     dup = False
     for _dirname in CHECK_DIRS:
@@ -46,17 +36,8 @@ def check_dup_files():
                 hx = get_file_hash(filepath)
                 if hx == None: continue
                 if hx in dupdict:
-                    logger.warning(f'filehash {hx} is duplicated, hash conflict in `{dupdict[hx]}` and `{filepath}`')
+                    warnings.warn(f'filehash {hx} is duplicated, hash conflict in `{dupdict[hx]}` and `{filepath}`')
                     dup = True
                 else:
                     dupdict[hx] = filepath
-    logger.debug('duplicated: %s', dup)
     assert(dup == False)
-
-
-def main(): 
-    check_total_png_count()
-    check_dup_files()
-
-if __name__ == '__main__':
-    main()
